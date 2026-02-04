275ba88a4265038467b4321c76ab5ba030b6a8c5
Covalentred <ayan.jayasekara@gmail.com>
2026-02-04 06:00:49 -0800

    Added Remaining tests for every state of tb_statemachine

--- task5/tb_statemachine.sv
+++ task5/tb_statemachine.sv
@@ -36,6 +36,9 @@ module tb_statemachine();
       resetb = 0;
       @(posedge slow_clock);
       resetb = 1;
+      pscore = 3'd0;
+      dscore = 3'd0;
+      pcard3 = 4'd0;
       @(posedge slow_clock);
     end
   endtask
@@ -69,7 +72,7 @@ module tb_statemachine();
       assert(dealer_win_light == 0)
       else $fatal("dealer_win_light not set to zero");
     end
-    endtask
+  endtask
 
   task automatic test_deal_2_cards();
     // Run the clock and verify that the correct signals go high and low
@@ -91,18 +94,176 @@ module tb_statemachine();
       else $fatal("FSM doesn't load pcard2 after dcard1");
 
       // Check that next state deals dealer's second card
-      @(posedge slow_clock)
+      @(posedge slow_clock);
       assert(load_dcard2)
       else $fatal("FSM doesn't load dcard2 after pcard2");
     end
-    endtask
+  endtask
+
+  task automatic test_player_win();
+    begin
+      do_reset();
+      // Step through state machine
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd9;
+      dscore = 4'd8;
+      @(posedge slow_clock);
+      #1
+      assert (player_win_light == 1 && dealer_win_light == 0)
+      else $fatal("Player doesn't Win Game");
+    end
+  endtask
+
+  task automatic test_dealer_win();
+    begin
+      do_reset();
+      // Step through state machine
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd8;
+      dscore = 4'd9;
+      @(posedge slow_clock);
+      #1
+      assert (player_win_light == 0 && dealer_win_light == 1)
+      else $fatal("Dealer doesn't Win Game");
+    end
+  endtask
+
+  task automatic test_tie();
+    begin
+      do_reset();
+      // Step through state machine
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd8;
+      dscore = 4'd8;
+      @(posedge slow_clock);
+      #1
+      assert (player_win_light == 1 && dealer_win_light == 1)
+      else $fatal("Game Wasn't a Tie");
+    end
+  endtask
+
+  task automatic test_natural();
+    begin
+      do_reset();
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd4;
+      dscore = 4'd8;
+      @(posedge slow_clock);
+      #1
+      assert(dealer_win_light == 1)
+      else $fatal("Natural didn't occur");
+    end 
+  endtask
+
+  task automatic test_player_draws_3rd_only();
+    begin
+      do_reset();
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd4;
+      dscore = 4'd7;
+      @(posedge slow_clock);
+      #1
+      assert(load_pcard3 == 1)
+      else $fatal("Player Doesn't Draw Hand");
+      
+      @(posedge slow_clock);
+      #1 
+      assert(load_dcard3 == 0)
+      else $fatal("Both Player and Dealer Draw");
+    end 
+  endtask
+
+  task automatic test_dealer_draws_3rd_only();
+    begin
+      do_reset();
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd6;
+      dscore = 4'd4;
+      @(posedge slow_clock);
+      #1
+      assert(load_dcard3 == 1)
+      else $fatal("Dealer Doesn't Draw Hand");
+      
+      @(posedge slow_clock);
+      #1 
+      assert(load_pcard3 == 0)
+      else $fatal("Both Player and Dealer Draw");
+    end 
+  endtask
+
+  task automatic test_both_dont_draw_3rd();
+    begin
+      do_reset();
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd6;
+      dscore = 4'd7;
+      @(posedge slow_clock);
+      #1
+      assert(load_dcard3 == 0)
+      else $fatal("Dealer Draws Hand");
+      
+      @(posedge slow_clock);
+      #1 
+      assert(load_pcard3 == 0)
+      else $fatal("Both Player and Dealer Draw");
+    end 
+  endtask
+
+  task automatic test_both_draw_3rd();
+    begin
+      do_reset();
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      pscore = 4'd4;
+      dscore = 4'd5;
+      @(posedge slow_clock);
+      #1
+      assert(load_pcard3 == 1)
+      else $fatal("Player Doesn't Draw Hand");
+      
+      pcard3 = 4'd4;
+      @(posedge slow_clock);
+      @(posedge slow_clock);
+      #1 
+      assert(load_dcard3 == 1)
+      else $fatal("Dealer Doesn't Draw Hand");
+    end 
+  endtask
 
   initial begin
     test_reset();
-
     #10
-
-    // test_deal_2_cards();
+    test_player_win();
+    #10
+    test_dealer_win();
+    #10 
+    test_tie();
+    #10
+    test_natural();
+    #10 
+    test_player_draws_3rd_only();
+    #10 
+    test_dealer_draws_3rd_only();
+    #10
+    test_both_dont_draw_3rd();
+    #10 
+    test_both_draw_3rd();
+    
     $display("ALL TESTS PASSED");
     $finish;
   end
